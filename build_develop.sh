d90d0ee7af0d4a1e974e055a34babc68f000fdab
Gerald Manipon <pymonger@gmail.com>
2019-09-04 14:21:34 +0000

    not pushing up develop builds as latest
    Trigger weekly builds off of master.

---
+++ build_develop.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+# usage: ./build_develop.sh hysds
+
+BASE_PATH=$(dirname "${BASH_SOURCE}")
+BASE_PATH=$(cd "${BASE_PATH}"; pwd)
+
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <org>"
+  echo "e.g.: $0 hysds"
+  exit 1
+fi
+ORG=$1
+RELEASE=develop
+BRANCH=develop
+
+# get uid and gid
+ID=$(id -u)
+GID=$(id -g)
+
+echo "RELEASE is $RELEASE"
+echo "ID is $ID"
+echo "GID is $GID"
+
+# pull latest docker.io centos, rabbitmq and elasticsearch
+docker pull docker.io/elasticsearch:1.7 || exit 1
+docker pull docker.io/rabbitmq:3-management || exit 1
+docker pull docker.io/centos:7 || exit 1
+docker tag docker.io/centos:7 docker.io/centos:latest || exit 1
+
+# make temp workspace
+TMP_DIR=$BASE_PATH/.tmp
+rm -rf $TMP_DIR
+mkdir $TMP_DIR
+cd $TMP_DIR
+
+# create artifacts directory
+IMG_DIR=${BASE_PATH}/images
+mkdir -p $IMG_DIR
+
+# build hysds/base
+echo "#############################"
+echo "Building hysds/base"
+echo "#############################"
+git clone --single-branch -b ${BRANCH} https://github.com/${ORG}/puppet-hysds_base.git hysds_base
+cd hysds_base
+./build_docker.sh ${RELEASE} ${ORG} ${BRANCH} || exit 1
+docker push hysds/base:${RELEASE} || exit 1
+docker push hysds/cuda-base:${RELEASE} || exit 1
+cd ..
+rm -rf hysds_base
+
+# build hysds/dev
+echo "#############################"
+echo "Building hysds/dev"
+echo "#############################"
+git clone --single-branch -b ${BRANCH} https://github.com/${ORG}/puppet-hysds_dev.git hysds_dev
+cd hysds_dev
+./build_docker.sh ${RELEASE} ${ORG} ${BRANCH} || exit 1
+docker push hysds/dev:${RELEASE} || exit 1
+docker push hysds/cuda-dev:${RELEASE} || exit 1
+cd ..
+rm -rf hysds_dev
+cd $BASE_PATH
+
+# hysds/redis
+echo "#############################"
+echo "Building hysds/redis"
+echo "#############################"
+docker build --rm --force-rm -t hysds/redis:${RELEASE} -f Dockerfile.hysds-redis . || exit 1
+docker push hysds/redis:${RELEASE} || exit 1
+
+# hysds/elasticsearch
+echo "#############################"
+echo "Building hysds/elasticsearch"
+echo "#############################"
+docker build --rm --force-rm -t hysds/elasticsearch:${RELEASE} -f Dockerfile.hysds-elasticsearch . || exit 1
+docker push hysds/elasticsearch:${RELEASE} || exit 1
+
+# hysds/rabbitmq
+echo "#############################"
+echo "Building hysds/rabbitmq"
+echo "#############################"
+docker build --rm --force-rm -t hysds/rabbitmq:${RELEASE} -f Dockerfile.hysds-rabbitmq . || exit 1
+docker push hysds/rabbitmq:${RELEASE} || exit 1
+
+# build worker hysds components
+echo "#######################################"
+echo "Building hysds/pge-base and hysds/verdi"
+echo "#######################################"
+cd $TMP_DIR
+git clone -b docker --single-branch https://github.com/${ORG}/puppet-verdi.git verdi
+cd verdi
+./build_docker.sh ${RELEASE} || exit 1
+cd ..
+rm -rf verdi
+cd $BASE_PATH
+for i in verdi pge-base; do
+  docker push hysds/${i}:${RELEASE} || exit 1
+  if [ "$i" = "pge-base" ]; then
+    docker push hysds/cuda-${i}:${RELEASE} || exit 1
+  fi
+done
+
+# export verdi
+cd $IMG_DIR
+docker save hysds/verdi:${RELEASE} > hysds-verdi-${RELEASE}.tar; echo "done saving"; pigz -f hysds-verdi-${RELEASE}.tar
+cd -
+
+# build hysds components
+for i in mozart metrics grq cont_int; do
+  echo "#############################"
+  echo "Building hysds/$i"
+  echo "#############################"
+  cd $TMP_DIR
+  git clone -b docker --single-branch https://github.com/${ORG}/puppet-${i} ${i}
+  cd ${i}
+  ./build_docker.sh ${RELEASE} || exit 1
+  cd ..
+  rm -rf ${i}
+  cd $BASE_PATH
+  docker push hysds/${i}:${RELEASE} || exit 1
+done
+
+# clean temp dir
+rm -rf $TMP_DIR
