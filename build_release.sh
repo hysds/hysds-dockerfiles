379d1fb5b7483e7da17bc2bf99f428267b4ddcae
Gerald Manipon <pymonger@gmail.com>
2020-05-26 18:59:21 -0700

    don't publish with latest tag

--- build_release.sh
+++ build_release.sh
@@ -50,12 +50,8 @@ echo "#############################"
 git clone --single-branch -b ${BRANCH} https://github.com/${ORG}/puppet-hysds_base.git hysds_base
 cd hysds_base
 ./build_docker.sh ${RELEASE} ${ORG} ${BRANCH} || exit 1
-docker tag hysds/base:${RELEASE} hysds/base:latest || exit 1
-docker tag hysds/cuda-base:${RELEASE} hysds/cuda-base:latest || exit 1
 docker push hysds/base:${RELEASE} || exit 1
 docker push hysds/cuda-base:${RELEASE} || exit 1
-docker push hysds/base:latest || exit 1
-docker push hysds/cuda-base:latest || exit 1
 cd ..
 rm -rf hysds_base
 
@@ -66,12 +62,8 @@ echo "#############################"
 git clone --single-branch -b ${BRANCH} https://github.com/${ORG}/puppet-hysds_dev.git hysds_dev
 cd hysds_dev
 ./build_docker.sh ${RELEASE} ${ORG} ${BRANCH} || exit 1
-docker tag hysds/dev:${RELEASE} hysds/dev:latest || exit 1
-docker tag hysds/cuda-dev:${RELEASE} hysds/cuda-dev:latest || exit 1
 docker push hysds/dev:${RELEASE} || exit 1
 docker push hysds/cuda-dev:${RELEASE} || exit 1
-docker push hysds/dev:latest || exit 1
-docker push hysds/cuda-dev:latest || exit 1
 cd ..
 rm -rf hysds_dev
 cd $BASE_PATH
@@ -81,27 +73,21 @@ echo "#############################"
 echo "Building hysds/redis"
 echo "#############################"
 docker build --rm --force-rm -t hysds/redis:${RELEASE} -f Dockerfile.hysds-redis . || exit 1
-docker tag hysds/redis:${RELEASE} hysds/redis:latest || exit 1
 docker push hysds/redis:${RELEASE} || exit 1
-docker push hysds/redis:latest || exit 1
 
 # hysds/elasticsearch
 echo "#############################"
 echo "Building hysds/elasticsearch"
 echo "#############################"
 docker build --rm --force-rm -t hysds/elasticsearch:1.7 -f Dockerfile.hysds-elasticsearch . || exit 1
-docker tag hysds/elasticsearch:1.7 hysds/elasticsearch:latest || exit 1
 docker push hysds/elasticsearch:1.7 || exit 1
-docker push hysds/elasticsearch:latest || exit 1
 
 # hysds/rabbitmq
 echo "#############################"
 echo "Building hysds/rabbitmq"
 echo "#############################"
 docker build --rm --force-rm -t hysds/rabbitmq:3-management -f Dockerfile.hysds-rabbitmq . || exit 1
-docker tag hysds/rabbitmq:3-management hysds/rabbitmq:latest || exit 1
 docker push hysds/rabbitmq:3-management || exit 1
-docker push hysds/rabbitmq:latest || exit 1
 
 # build worker hysds components
 echo "#######################################"
@@ -115,20 +101,15 @@ cd ..
 rm -rf verdi
 cd $BASE_PATH
 for i in verdi pge-base; do
-  docker tag hysds/${i}:${RELEASE} hysds/${i}:latest || exit 1
   docker push hysds/${i}:${RELEASE} || exit 1
-  docker push hysds/${i}:latest || exit 1
   if [ "$i" = "pge-base" ]; then
-    docker tag hysds/cuda-${i}:${RELEASE} hysds/cuda-${i}:latest || exit 1
     docker push hysds/cuda-${i}:${RELEASE} || exit 1
-    docker push hysds/cuda-${i}:latest || exit 1
   fi
 done
 
 # export verdi
 cd $IMG_DIR
 docker save hysds/verdi:${RELEASE} > hysds-verdi-${RELEASE}.tar; echo "done saving"; pigz -f hysds-verdi-${RELEASE}.tar
-docker save hysds/verdi:latest > hysds-verdi-latest.tar; echo "done saving"; pigz -f hysds-verdi-latest.tar
 cd -
 
 # build hysds components
@@ -143,9 +124,7 @@ for i in mozart metrics grq cont_int; do
   cd ..
   rm -rf ${i}
   cd $BASE_PATH
-  docker tag hysds/${i}:${RELEASE} hysds/${i}:latest || exit 1
   docker push hysds/${i}:${RELEASE} || exit 1
-  docker push hysds/${i}:latest || exit 1
 done
 
 # clean temp dir
